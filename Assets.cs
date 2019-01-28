f31bea1158b7de042647174a5ce00f24c8526151
Jean-David Moisan <vynecompiler@gmail.com>
2019-01-28 08:41:54 -0500

    Use SpriteFont for testing

--- Assets.cs
+++ Assets.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended.BitmapFonts;
 using SpriteFontPlus;
 
@@ -12,7 +13,9 @@ namespace AposGameStarter
         }
         public static void LoadFonts(ContentManager Content) {
             Font = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Content.RootDirectory + "/Fonts/SourceCodePro-Medium.ttf"), 30);
+            Font2 = Content.Load<SpriteFont>("Fonts/SourceCodePro");
         }
         public static DynamicSpriteFont Font;
+        public static SpriteFont Font2;
     }
 }
